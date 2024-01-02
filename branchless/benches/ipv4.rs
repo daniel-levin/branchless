d357d5f36782fd4205fa037d3020fa7e4b2a23c3
Daniel Levin <daniellevin2607@gmail.com>
2024-01-02 15:53:03 +0200

    Added single inet4 parse benchmark.

--- branchless/benches/ipv4.rs
+++ branchless/benches/ipv4.rs
@@ -9,6 +9,20 @@ use test::Bencher;
 static TEST_DATA: [&'static str; 10_000] = include!("../testdata/random");
 
 #[bench]
+fn bench_parse_inet4_single(bencher: &mut Bencher) {
+    bencher.iter(|| {
+        branchless::ip::parse_ipv4("127.0.0.1").unwrap();
+    });
+}
+
+#[bench]
+fn bench_parse_inet4_default_single(bencher: &mut Bencher) {
+    bencher.iter(|| {
+        std::net::Ipv4Addr::from_str("127.0.0.1").unwrap();
+    });
+}
+
+#[bench]
 fn bench_parse_inet4_sse2(bencher: &mut Bencher) {
     bencher.iter(|| {
         TEST_DATA.map(|x| branchless::ip::parse_ipv4(x).unwrap());
