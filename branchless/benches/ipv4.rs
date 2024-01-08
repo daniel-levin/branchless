ee745561d07d44acfbd5a6782a354fde829d835b
Daniel Levin <daniellevin2607@gmail.com>
2024-01-08 10:00:30 +0200

    Fixed clippy warnings.

--- branchless/benches/ipv4.rs
+++ branchless/benches/ipv4.rs
@@ -11,14 +11,14 @@ static TEST_DATA: [&str; 10_000] = include!("../testdata/random");
 #[bench]
 fn bench_parse_inet4_single(bencher: &mut Bencher) {
     bencher.iter(|| {
-        branchless::ip::parse_ipv4("127.0.0.1").unwrap();
+        let _ = branchless::ip::parse_ipv4("127.0.0.1").unwrap();
     });
 }
 
 #[bench]
 fn bench_parse_inet4_default_single(bencher: &mut Bencher) {
     bencher.iter(|| {
-        std::net::Ipv4Addr::from_str("127.0.0.1").unwrap();
+        let _ = std::net::Ipv4Addr::from_str("127.0.0.1").unwrap();
     });
 }
 
@@ -26,7 +26,7 @@ fn bench_parse_inet4_default_single(bencher: &mut Bencher) {
 fn bench_parse_inet4_sse2(bencher: &mut Bencher) {
     bencher.iter(|| {
         TEST_DATA.iter().for_each(|x| {
-            branchless::ip::parse_ipv4(x);
+            let _ = branchless::ip::parse_ipv4(x);
         });
     });
 }
@@ -35,7 +35,7 @@ fn bench_parse_inet4_sse2(bencher: &mut Bencher) {
 fn bench_parse_inet4_default(bencher: &mut Bencher) {
     bencher.iter(|| {
         TEST_DATA.iter().for_each(|x| {
-            std::net::Ipv4Addr::from_str(x);
+            let _ = std::net::Ipv4Addr::from_str(x);
         });
     });
 }
