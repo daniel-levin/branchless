bd6fcc70757713c88a08599f885d6779e88d8c6a
Daniel Levin <daniellevin2607@gmail.com>
2024-01-21 15:47:02 +0200

    add docstrings

--- branchless_core/src/ip.rs
+++ branchless_core/src/ip.rs
@@ -5,8 +5,11 @@
 pub use crate::raw::sse2::parse_ipv4;
 
 /// Coarse-grained error for [parse_ipv4].
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, thiserror::Error)]
 pub enum Ipv4ParseError {
+    /// Expected a string with length between 7 and 15.
     WrongLength,
+
+    /// Otherwise invalid input.
     Invalid,
 }
